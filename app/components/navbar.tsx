b5a442dc786eb92c34778f3b96f2d5f9330e7821
ngtrthinh <ng.tr.thinh@gmail.com>
2023-07-27 16:51:49 +0700

    feat: hover state for navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -27,7 +27,10 @@ export default function Navbar() {
   return (
     <nav className='font-inter bg-[#fafffa] text-dark-grayish-blue h-8 hidden xl:flex text-center items-center gap-x-12'>
       {navLinks.map((item) => (
-        <NavLink key={`navlink-${item.to}`} to={item.to}>
+        <NavLink
+          key={`navlink-${item.to}`}
+          to={item.to}
+          className='hover:text-soft-orange'>
           {item.display}
         </NavLink>
       ))}
